c8f77175ab62eafab876a8a92c46ab450950e2ef
mark_kim <70873668+markkim340@users.noreply.github.com>
2024-02-07 15:57:44 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,7 +1,6 @@
 function solution(rank, attendance) {
     var answer = 0;
     const filter = [...rank].filter((el,i) => attendance[i]===true).sort((a,b)=>a-b);
-    console.log(filter)
 
     answer += rank.indexOf(filter[0])*10000
     answer += rank.indexOf(filter[1])*100
